bd473d2e7cd641e73a8c6a4db25365e45364206e
ruba dayy <ruba.dayyeh@gmail.com>
2024-03-09 16:46:35 +0200

    Adding the Price attribute to Product Class

--- Domain/ProductManagement/Product.cs
+++ Domain/ProductManagement/Product.cs
@@ -1,21 +1,22 @@
-﻿using System;
+﻿using PieShopInventoryManagement.Domain.General;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PieShopInventoryManagement
+namespace PieShopInventoryManagement.Domain.ProductManagement
 {
     public class Product
     {
         private int productID;
-        private string productName = String.Empty;
-        private string? productDescription;
+        private string productName = string.Empty;
+        private string productDescription;
 
         private int maxItemsInStock = 0;
 
-        
+
 
         //PROPERTIES 
         public int ProductID
@@ -26,17 +27,19 @@ namespace PieShopInventoryManagement
         public string ProductName
         {
             get { return productName; }
-            set {
-                productName = (value.Length > 50) ? value[..50] : value;
+            set
+            {
+                productName = value.Length > 50 ? value[..50] : value;
             }
         }
         public string ProductDescription
         {
             get { return productDescription; }
-            set {
+            set
+            {
                 if (value == null)
                 {
-                    productDescription = String.Empty;
+                    productDescription = string.Empty;
                 }
                 else
                 {
@@ -49,25 +52,28 @@ namespace PieShopInventoryManagement
         public UnitType UnitType { get; set; }
         public int AmountInStock { get; private set; }
         public bool isBelowStockThreshold { get; private set; }
-
-        public Product(int id,string name) { //using properties
-            ProductID = id;        
+        public Price Price { get;  set; }
+        public Product(int id, string name)
+        { //using properties
+            ProductID = id;
             ProductName = name;
         }
-        public Product(int id):this(id, String.Empty){ }
-        
+        public Product(int id) : this(id, string.Empty) { }
+
         public Product()
         {
             maxItemsInStock = 100;
-            ProductName = String.Empty;
+            ProductName = string.Empty;
         }
-        public Product(int id, string name,string? description,UnitType unitType,int maxItemsInStock)
-        { 
+        public Product(int id, string name, string? description,Price price, UnitType unitType, int maxItemsInStock)
+        {
             ProductID = id;
             ProductName = name;
             ProductDescription = description;
+            Price = price;
             UnitType = unitType;
             this.maxItemsInStock = maxItemsInStock;
+            
 
             UpdateLowStock();
         }
@@ -81,19 +87,19 @@ namespace PieShopInventoryManagement
         }
         public void IncreaseAmountInStock(int amount)
         {
-           
-            if ((AmountInStock+amount) <= maxItemsInStock)
+
+            if (AmountInStock + amount <= maxItemsInStock)
             {
                 AmountInStock += amount;
             }
             else
             {
                 AmountInStock = maxItemsInStock; // we only store the possible items overstock isn't stored
-                Log($"{CreateSimpleProductRepresentation} stock overflow. {(AmountInStock + amount) - AmountInStock} item(s) ordered that couldn't be stored");
+                Log($"{CreateSimpleProductRepresentation} stock overflow. {AmountInStock + amount - AmountInStock} item(s) ordered that couldn't be stored");
             }
-            UpdateLowStock() ;
+            UpdateLowStock();
         }
-        private void DecreaseAmountInStock(int items,String reason) // accesses in a class level .
+        private void DecreaseAmountInStock(int items, string reason) // accesses in a class level .
         {
             if (items <= AmountInStock)
             {
@@ -104,12 +110,12 @@ namespace PieShopInventoryManagement
                 AmountInStock = 0;
             }
             UpdateLowStock(); //THIS WILL SEND NOTIFICATION IF STOCK LOW
-            Log(reason); 
+            Log(reason);
         }
 
         public void UseProduct(int items)  //this will allow to keep track of the amount in stock whenver items are used and decremented.
         {
-            if (items<= AmountInStock)
+            if (items <= AmountInStock)
             {
                 AmountInStock -= items;
                 UpdateLowStock();
@@ -131,7 +137,7 @@ namespace PieShopInventoryManagement
                 isBelowStockThreshold = false;
             }
         }
-        private void Log(String message)
+        private void Log(string message)
         {
             Console.WriteLine(message);
         }
@@ -142,8 +148,8 @@ namespace PieShopInventoryManagement
         }
         public string DisplayDetailsFull()  //this will provide and ALERT IF STOCK LOW!
         {
-            StringBuilder sb =new StringBuilder();
-            sb.Append($"{productID} {productName} \n{productDescription} \n{AmountInStock} items in stock.");
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"{productID} {productName} \n{productDescription} \n {Price}\n{AmountInStock} items in stock.");
             if (isBelowStockThreshold)
             {
                 sb.Append("\n !!STOCK LOW!!");
